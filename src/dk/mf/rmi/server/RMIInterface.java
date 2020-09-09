4535db43c9b8be3fc6166c7d480097d1f2443882
MortenFeldtStudent <cph-mf227@cphbusiness.dk>
2020-09-09 20:44:33 +0200

    Assignment 2-1

---
+++ src/dk/mf/rmi/server/RMIInterface.java
@@ -0,0 +1,17 @@
+package dk.mf.rmi.server;
+/**
+ * @author Dora Di
+ */
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface RMIInterface extends Remote {
+    // Simple arithmetics
+    public double calculate(int a, int b, char c) throws RemoteException;
+
+    // Get current date
+    public java.util.Date getDate() throws RemoteException;
+
+}
+
---
+++ src/dk/mf/rmi/server/RMIRegistry.java
@@ -0,0 +1,24 @@
+package dk.mf.rmi.server;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class RMIRegistry {
+    public static Registry registry;
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("RMI registry");
+
+            registry = LocateRegistry.getRegistry("localhost", 1099);
+            String[] boundNames = registry.list();
+            System.out.println("Names bound to RMI registry at localhost:");
+            for (String name : boundNames) {
+                System.out.println("\t" + name);
+            }
+        } catch (Exception e) {
+            System.err.println("Exception:" + e);
+        }
+    }
+}
+
